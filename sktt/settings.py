20471e482fa1daf58b52e020b503c478595640ff
subash2102 <subashmariappan2004@gmail.com>
2026-05-03 13:23:38 +0530

    fix templates issue

--- sktt/settings.py
+++ sktt/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = "sktt.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates"],
+        "DIRS": [os.path.join(os.getcwd(), "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
