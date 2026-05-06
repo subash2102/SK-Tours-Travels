9b6847e7d03af741aeef7863a552c1232e14affe
subash2102 <subashmariappan2004@gmail.com>
2026-05-06 13:53:36 +0530

    fixed whitenoise

--- sktt/settings.py
+++ sktt/settings.py
@@ -113,11 +113,22 @@ USE_I18N = True
 USE_TZ = True
 
 
+# STATIC_URL = "static/"
+# STATICFILES_DIRS = [BASE_DIR / "static"]
+# STATIC_ROOT = BASE_DIR / "staticfiles"
+# if os.getenv("DJANGO_DEBUG", "1") != "1":
+#     STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
 if os.getenv("DJANGO_DEBUG", "1") != "1":
-    STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+    STORAGES = {
+        "staticfiles": {
+            "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        },
+    }
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
